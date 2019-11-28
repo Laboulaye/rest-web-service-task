33e5bc8f3971ecb14527473cc2723c1c8bde28b7
Maksim Demianov <maks.demianov@gmail.com>
2019-11-28 20:54:02 +0300

    add swagger and description HTTP methods

--- src/main/java/com/study/restwebservicetask/Controller/ContactController.java
+++ src/main/java/com/study/restwebservicetask/Controller/ContactController.java
@@ -2,12 +2,16 @@ package com.study.restwebservicetask.Controller;
 
 import com.study.restwebservicetask.Dao.ContactDao;
 import com.study.restwebservicetask.Model.Contact;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Api
 @RequestMapping("/users/{userId}")
 public class ContactController {
 
@@ -15,34 +19,52 @@ public class ContactController {
     ContactDao contactDao;
 
     @GetMapping("/contacts")
-        public List<Contact> getAllContacts(@PathVariable("userId") String userId){
-            return contactDao.getAllContacts(userId);
+    @ApiOperation(value = "View a list of available user contacts", response = List.class)
+    public List<Contact> getAllContacts(@PathVariable("userId") String userId){
+        return contactDao.getAllContacts(userId);
     }
 
     @PostMapping("/contacts")
-    public Contact addContact(@PathVariable("userId") String userId, @RequestBody Contact contact){
+    @ApiOperation(value = "Add contact")
+    public Contact addContact(@ApiParam(value = "User Id to add user contact object", required = true)
+                                  @PathVariable("userId") String userId,
+                              @ApiParam(value = "Contact object for add", required = true)
+                                    @RequestBody Contact contact){
         System.out.println("(Service Side) Creating contact: #" + contact.getId() + " of user: #" + userId);
         return contactDao.addContact(userId, contact);
     }
 
     @GetMapping("/contacts/{contactId}")
-    public Contact getContact(@PathVariable("userId") String userId, @PathVariable("contactId") String contactId){
+    @ApiOperation(value = "Get contact by Id")
+    public Contact getContact(@ApiParam(value = "User Id to get user contact object", required = true)
+                                  @PathVariable("userId") String userId,
+                              @ApiParam(value = "Contact Id from which contact object will retrieve", required = true)
+                                    @PathVariable("contactId") String contactId){
         return contactDao.getContact(userId, contactId);
     }
 
     @PutMapping("/contacts")
-    public Contact editContact(@PathVariable("userId") String userId, @RequestBody Contact contact){
+    @ApiOperation(value = "Update contact")
+    public Contact editContact(@ApiParam(value = "User Id to update user contact object", required = true)
+                                   @PathVariable("userId") String userId,
+                               @ApiParam(value = "Update contact object", required = true)
+                               @RequestBody Contact contact){
         return contactDao.editContact(userId, contact);
     }
 
     @DeleteMapping("/contacts/{contactId}")
-    public void deleteContact(@PathVariable("userId") String userId, @PathVariable("contactId") String contactId){
+    @ApiOperation(value = "Delete contact")
+    public void deleteContact(@ApiParam(value = "User Id to delete user contact object", required = true)
+                                  @PathVariable("userId") String userId,
+                              @ApiParam(value = "Contact Id from which contact object will delete", required = true)
+                              @PathVariable("contactId") String contactId){
         System.out.println("(Service Side) Deleting contact: #" + contactId + " of user: #" + userId );
         contactDao.deleteContact(userId, contactId);
     }
 
     @GetMapping("/search")
-    public Contact findContactByPhone(@RequestParam(name="phone") String phone){
+    @ApiOperation(value = "Find contact by phone")
+    public Contact findContactByPhone(@ApiParam(value = "Contact phone for search contact", required = true) @RequestParam(name="phone") String phone){
         return contactDao.findContactByPhone(phone);
     }
 
--- src/main/java/com/study/restwebservicetask/Controller/UserController.java
+++ src/main/java/com/study/restwebservicetask/Controller/UserController.java
@@ -3,52 +3,67 @@ package com.study.restwebservicetask.Controller;
 
 import com.study.restwebservicetask.Dao.UserDao;
 import com.study.restwebservicetask.Model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Api
 public class UserController {
 
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ApiOperation(value = "Home page")
     public String welcome(){
         return "Welcome to Homepage REST web App";
     }
 
     @GetMapping("/users")
+    @ApiOperation(value = "View a list of available users", response = List.class)
     public List<User> getUsers(){
         return userDao.getAllUsers();
     }
 
     @PostMapping("/users")
-    public User addUser(@RequestBody User user){
+    @ApiOperation(value = "Add user")
+    public User addUser(@ApiParam(value = "User object for add", required = true)
+                            @RequestBody User user){
         System.out.println("(Service Side) Creating User: " + user.getId());
         return userDao.addUser(user);
     }
 
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable("id") String id){
+    @ApiOperation(value = "Get user by Id")
+    public User getUser(@ApiParam(value = "User Id from which user object will retrieve", required = true)
+                            @PathVariable("id") String id){
         return userDao.getUser(id);
     }
 
     @PutMapping("/users")
-    public User editUser(@RequestBody User user){
+    @ApiOperation(value = "Update user")
+    public User editUser(@ApiParam(value = "Update user object", required = true)
+                             @RequestBody User user){
         System.out.println("(Service Side) Editing User: " + user.getId());
         return userDao.editUser(user);
     }
 
-    @DeleteMapping("users/{id}")
-    public void deleteUser(@PathVariable("id") String id){
-        System.out.println("(Service Side) Deleting User: " + id);
-        userDao.deleteUser(id);
+    @DeleteMapping("users/{userId}")
+    @ApiOperation(value = "Delete user")
+    public void deleteUser(@ApiParam(value = "User Id from which user object will delete", required = true)
+                               @PathVariable("userId") String userId){
+        System.out.println("(Service Side) Deleting User: " + userId);
+        userDao.deleteUser(userId);
     }
 
     @GetMapping("/search")
-    public User findUserByName(@RequestParam(name="name") String name){
+    @ApiOperation(value = "Find user by name")
+    public User findUserByName(@ApiParam(value = "User name for search user info", required = true) @RequestParam(name="name") String name){
         return userDao.findUserByName(name);
     }
 
